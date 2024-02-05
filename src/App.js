775ff853201f9c46326e7fef30e67af4e4a8b2e1
hardik-fabriclake <154807884+hardik-fabriclake@users.noreply.github.com>
2024-02-05 13:04:00 +0530

    FO-6 (task) react counter project dependency upgrade 16 to 17

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Counter from "./components/Counter";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Counter />
